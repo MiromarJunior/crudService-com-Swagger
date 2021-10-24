e318fd3546c08d55edd14cd53a06014eb8e8b5b3
MIromar Junior <miromarjunior@gmail.com>
2021-10-24 15:53:34 -0300

    CRUD com Swagger

---
+++ crud-service/src/test/java/com/crud/service/CrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
